96aa8f329f37b262dcdd26e597b1168d2c8661e5
Abhinav Kumar <abhinavsiwan@gmail.com>
2016-06-23 12:52:16 -0700

    Refactored the code with built in underscore methods

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+//need to refactor the code and use underscore builtin functions
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -22,13 +25,7 @@ app.get('/todos', function (res, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if(todo.id === todoID) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoID});
 
 	if(matchedTodo) {
 		res.json(matchedTodo);
@@ -44,11 +41,16 @@ takes any json request that comes in, express will parse it and we will be
 able to access it within the req.body parameter.
 */
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send("Bad Data Format, Can't be processed");
+	}
+
+	body.description = body.description.trim();
 	body.id = todoIDNext++;
 
 	todos.push(body);
-	
 	res.json(body);
 });
 
