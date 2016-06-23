f23664525da31edd6d398717aa8c6af4e6a82dd5
Abhinav Kumar <abhinavsiwan@gmail.com>
2016-06-23 15:12:17 -0700

    Added /GET /todos/key=value qyeryParams

--- server.js
+++ server.js
@@ -17,9 +17,24 @@ app.get('/', function (req, res) {
 	res.send('Todo API started');
 });
 
-//GET /todos
-app.get('/todos', function (res, res) {
-	res.json(todos);
+//GET /todos?completed=false&q=work
+app.get('/todos', function (req, res) {
+	//returns the query as an object
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+	res.json(filteredTodos);
 });
 
 //GET /todos/:id
