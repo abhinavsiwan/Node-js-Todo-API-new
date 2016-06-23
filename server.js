99dd622fa92e7dc8defd715c8f015637c01a42d3
Abhinav Kumar <abhinavsiwan@gmail.com>
2016-06-23 12:31:43 -0700

    Added GET /todos/:id and POST /todos api methods

--- server.js
+++ server.js
@@ -1,22 +1,55 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'walk the dog',
-	completed: false
-}, {
-	id: 2,
-	description: 'walk the cat',
-	completed: false
-}];
+//todos array to hold all the todo items
+var todos = [];
+var todoIDNext = 1;
+
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
 	res.send('Todo API started');
 });
 
+//GET /todos
 app.get('/todos', function (res, res) {
 	res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if(todo.id === todoID) {
+			matchedTodo = todo;
+		}
+	});
+
+	if(matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send("No Entry found");
+	}
+});
+
+//POST /todos
+/* 
+For post, we need to download another module called body-parser which
+takes any json request that comes in, express will parse it and we will be
+able to access it within the req.body parameter.
+*/
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	body.id = todoIDNext++;
+
+	todos.push(body);
+	
+	res.json(body);
 });
 
 app.listen(PORT, function () {
